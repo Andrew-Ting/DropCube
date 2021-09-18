ef556f0ff0dcc874a4aa7445a167b8eb20576a1c
Andrew Ting <andrewjamesting@yahoo.com>
2021-09-17 23:59:42 -0700

    Added transition scenes in UI

---
+++ Assets/Scripts/Scene Navigation/SceneSwitcher.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+[RequireComponent(typeof(Button))]
+public class SceneSwitcher : MonoBehaviour
+{
+    private enum SceneType {
+        MainMenu,
+        GameplayScene,
+        GameOver
+    };
+
+    [SerializeField] 
+    private SceneType destinationScene;
+
+    public Animator screenTransition;
+    public void Awake() {
+        Button thisButton = GetComponent<Button>();
+       thisButton.onClick.AddListener(() => {
+            StartCoroutine(loadScene());
+       });
+    }
+    private IEnumerator loadScene()
+    {
+        screenTransition.SetTrigger("start");
+        yield return new WaitForSecondsRealtime(1f);
+        SceneManager.LoadScene(destinationScene.ToString());
+        yield return false;
+    }
+}
